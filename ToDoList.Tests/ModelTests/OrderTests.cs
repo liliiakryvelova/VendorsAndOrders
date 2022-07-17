7324ccd15fb9079fdcfc9e0a30e0e43baf7edf7e
liliia87 <lilia.krivelyova@gmail.com>
2022-07-17 13:39:57 -0700

    GetId_OrderssInstantiateWithAnIdAndGetterReturns_Int

--- ToDoList.Tests/ModelTests/OrderTests.cs
+++ ToDoList.Tests/ModelTests/OrderTests.cs
@@ -85,17 +85,24 @@ namespace ToDoList.Tests
       string vendorname = "Vendor 1";
       string description = "Sell apples";
       Vendor newVendor1 = new Vendor(vendorname, description);
+
       string title1 = "Bread.";
       string orderDescription1 = "Sell apples";
       string price1 = "10";
       Order newOrder1 = new Order(title1, orderDescription1, price1);
       newVendor1.AddOrder(newOrder1);
 
+      string title2 = "Bread.";
+      string orderDescription2 = "Sell apples";
+      string price2 = "10";
+      Order newOrder2 = new Order(title2, orderDescription2, price2);
+      newVendor1.AddOrder(newOrder2);
+
       //Act
-      int result = newOrder1.Id;
+      Order result = Order.Find(newVendor1, newOrder1.Id);
 
       //Assert
-      Assert.AreEqual(1, result);
+      Assert.AreEqual(newOrder1, result);
     }
 
 //     [TestMethod]
